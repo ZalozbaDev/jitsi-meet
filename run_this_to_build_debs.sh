1798bf358ff2d51596c2a0492f2b87d9a0ad7cea
Daniel Zoba <daniel.sobe@sorben.com>
2022-04-01 13:12:48 +0200

    add doc

--- run_this_to_build_debs.sh
+++ run_this_to_build_debs.sh
@@ -7,3 +7,5 @@ dpkg-buildpackage -A -rfakeroot -us -uc -tc -d
 echo "=================================================="
 echo ".deb files are now in parent folder"
 
+echo "You need to copy the relevant ones to the docker-jitsi-meet repo for including them into the container."
+
