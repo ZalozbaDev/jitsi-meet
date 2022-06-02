899fb8d04a69b5a100a4264d47984be7774930f6
Daniel Zoba <daniel.sobe@sorben.com>
2022-06-02 14:54:16 +0200

    fix and document how to validate JSON

--- run_this_to_build_debs.sh
+++ run_this_to_build_debs.sh
@@ -2,7 +2,12 @@
 
 echo "Check the documentation in lang/README.md (on Github) how to update the translation files."
 echo "e.g. cd lang && node update-translation.js main-hsb.json"
-
+echo
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo "Validate JSON format after update, before building!"
+echo "Use an online validator like: https://jsonformatter.org/"
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo
 
 echo 'Run "npm install" at least once.'
 make
